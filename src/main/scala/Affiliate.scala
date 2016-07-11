467ff165ca12e8f0dea968ccbf97d2f100f05d46
FScoward <endlick@gmail.com>
2016-07-11 21:22:48 +0900

    add affiliate

--- src/main/scala/Affiliate.scala
+++ src/main/scala/Affiliate.scala
@@ -14,7 +14,8 @@ object Affiliate {
     Affiliate("NOGIBINGO! 5 Blu-ray BOX", "http://amzn.to/1Nppxrm"),
     Affiliate("悲しみの忘れ方 Documentary of 乃木坂46 Blu-ray コンプリートBOX(4枚組)(完全限定生産)", "http://amzn.to/1NppKL9"),
     Affiliate("初森ベマーズ Blu-ray SPECIAL BOX(ロゴ入りオリジナルトートバック付)(先着予約購入特典:メンバーメッセージ入り生写真12枚セット)[Blu-ray]", "http://amzn.to/23jaISM"),
-    Affiliate("乃木坂46 生駒里奈ファースト写真集『君の足跡』", "http://amzn.to/1QDKHtY")
+    Affiliate("乃木坂46 生駒里奈ファースト写真集『君の足跡』", "http://amzn.to/1QDKHtY"),
+    Affiliate("乃木坂46 3rd YEAR BIRTHDAY LIVE 2015.2.22 SEIBU DOME", "http://amzn.to/29H2YWW")
   )
 
   def getOne = affiliates(Random.nextInt(affiliates.length - 1))
